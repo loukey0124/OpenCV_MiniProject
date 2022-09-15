1f3b0a822cca38cc26b8f9e48e9b7097be2fb3a3
donghyuk12 <tto108@naver.com>
2022-09-15 16:56:30 +0900

    0915-1656

---
+++ testFile/flaskserver.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+import os
+from werkzeug.utils import secure_filename
+import flaskserver    
+
+app = Flask(__name__, static_folder='faces', template_folder='template')
+
+@app.route('/')
+def test_1():
+    
+
+
+if __name__ == '__main__':
+    app.run(
+    host="0.0.0.0",
+    port=5000,
+    debug=True)
